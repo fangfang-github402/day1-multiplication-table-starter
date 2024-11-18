8bd948e34de8071c67c3ef1c785cb05d19bf4203
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-18 16:28:06 +0800

    [MultiplicationTable]refactor,rename builder

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -6,12 +6,12 @@ public class MultiplicationTableBuilder {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int start = 2;
         int end = 4;
-        String multiplicationTable = builder.build(start, end);
+        String multiplicationTable = builder.genMultiplicationTable(start, end);
 
         System.out.println(multiplicationTable);
     }
 
-    public String build(int start, int end) {
+    public String genMultiplicationTable(int startNumber,int endNumber) {
         return "";
     }
 }
